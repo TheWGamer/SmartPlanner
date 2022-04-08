36eaf499a55c0ff3853275561add2c53482bcc8e
Chase Webb <chwebb2002@gmail.com>
2022-04-07 20:07:58 -0500

    modified:   .gitignore

--- init.c
+++ init.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void init() {
+    FILE *savings, *debts, *investments;
+
+    char sfile[1024] = "savings";
+    char dfile[1024] = "debts";
+    char ifile[1024] = "investments";
+
+    strcat(sfile, username);
+    strcat(dfile, username);
+    strcat(ifile, username);
+
+     printf("%s\n", sfile);
+
+    return;
+}
--- main.c
+++ main.c
@@ -7,16 +7,22 @@
 int main() {
     int input;
 
-    printf("Would you like to login to an existing account or create a new one?\n");
+    printf("\nWould you like to login to an existing account or create a new one?\n");
     printf("1. Log in\n2. Create New\n");
         printf("Selection: ");
         scanf("%d", &input); getc(stdin);
-        if (input == 1)
-            login();
+        if (input == 1) {
+            if (!login()) {
+                printf("Closing Program...\n");
+                exit(1);
+            }
+        }
         else
             new();
 
     printf("Running init system...\n");
+    init();
+
     printf("Welcome to SmartPlanner, %s!\n", username);
 
     return 0;
